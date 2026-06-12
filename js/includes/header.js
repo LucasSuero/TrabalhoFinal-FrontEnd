30f4ece6079af96dfa485b69233d1f720d3c0e1a
Leonardo <leokenjits@gmail.com>
2026-06-11 21:37:46 -0300

    refactor: Mudei o cabeçalho para o JavaScript
    O cabeçalho no javascript facilita as alterações futuras, acelerando o processo de criação da aplicação, também alterei o conceito do css do portal e adicionei variaveis padrão para os valores de cores principais do site

---
+++ js/includes/header.js
@@ -0,0 +1,15 @@
+document.getElementById("header").innerHTML = `
+    <div class="topo">
+        <h1>Portal Acadêmico</h1>
+        <h2>Feito para modernizar o sistema acadêmico</h2>
+    </div>
+
+    <div class="selecao">
+        <a href="/index.html">Início</a>
+        <a href="/pages/cadastro.html">Cadastro Alunos</a>
+        <a href="/pages/notas.html">Notas</a>
+        <a href="/pages/relatorios.html">Relatórios</a>
+        <a href="/pages/sobre.html">Sobre</a>
+    </div>
+`;
+console.log("Header Carregado!");
